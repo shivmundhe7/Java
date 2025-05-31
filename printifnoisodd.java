4ee21b3337fb83814f7453d74b1d3ab5dd6eb532
shivmundhe7 <shivmundhe16@gmail.com>
2025-05-31 06:11:49 +0530

    Learned If else

---
+++ printifnoisodd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class printifnoisodd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("The Number Is Even");
+        } else {
+            System.out.println("The Number Is Odd");
+        }
+
+        sc.close();
+    }
+}
