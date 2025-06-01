1f96027141a59a6799df395748ba9d6b847df82c
shivmundhe7 <shivmundhe16@gmail.com>
2025-06-01 14:42:34 +0530

    Java

---
+++ loops.java
@@ -0,0 +1,12 @@
+public class loops {
+    
+    public static void main(String[] args) {
+        int counter = 0;
+        while(counter < 100) {
+            System.out.println("Hello World");
+            counter++;
+        }
+        System.out.println("printed HW 100x");
+    }
+    
+}
---
+++ sumofnaturalno.java
@@ -0,0 +1,18 @@
+// Sum Of First n natural Numbers
+import java.util.*;
+
+public class sumofnaturalno{
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          int n = sc.nextInt();
+          int sum = 0;
+
+          int i=1;
+          while(i <=n) {
+            sum += i;
+            i++;
+          }
+
+          System.out.println("sum is : " + sum);
+    }
+}
