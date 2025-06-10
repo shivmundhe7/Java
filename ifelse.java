eca593f3427768ee2ed9f3173a41da6c90b0c343
shivmundhe7 <shivmundhe16@gmail.com>
2025-06-10 16:50:06 +0530

    java

--- ifelse.java
+++ ifelse.java
@@ -3,12 +3,11 @@ public class ifelse {
     int age = 16; 
     if(age >= 18){
         System.out.println("adult : drive, vote");
-        
+
     }
     if(age > 13 && age < 18){
         System.out.println("teenager");
     }
-
     else{
         System.out.println("Not Adult");
     }
