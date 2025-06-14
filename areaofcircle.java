b3882e65b8415ac41cf1a884fb4506951ae86133
shivmundhe7 <shivmundhe16@gmail.com>
2025-06-14 14:38:05 +0530

    Java

--- areaofcircle.java
+++ areaofcircle.java
@@ -9,15 +9,3 @@ public class areaofcircle{
         }
     }
 }
-//Revision Of Code 
-//import java.util.Scanner;
-//public class areaofcircle{
-//  public static void main(String[] args) {
-//      try (Scanner sc = new Scanner(System.in)) {
-//          float rad = sc.nextFloat();
-//          float area = 3.14f * rad * rad;
-//            System.out.println(area);
-//        }
-//   }
-//
-//}
