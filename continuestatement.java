1759354ef79462c52fedef83901dd5410a81a328
shivmundhe7 <shivmundhe16@gmail.com>
2025-07-14 16:24:10 +0530

    java

--- continuestatement.java
+++ continuestatement.java
@@ -3,9 +3,7 @@ public class continuestatement {
     public static void main(String[] args) {
             for (int i=1; i<=5; i++) {
         if(i == 3) {  
-
-        
-          
+            
         }
     }
     }
